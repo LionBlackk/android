03eb9323027525c50fd4d2959d93e18d4200f356
HoangVo <vophuochoang93@gmail.com>
2024-02-18 20:17:06 +0700

    baitap4: figma to android

---
+++ baitap4/app/src/main/java/exportkit/figma/login___sign_up_activity.java
@@ -0,0 +1,120 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		login___sign_up
+	 *	@date 		Sunday 18th of February 2024 08:31:39 AM
+	 *	@title 		Mockups
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package exportkit.figma;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.baitap4.R;
+
+	public class login___sign_up_activity extends Activity {
+
+	
+	private View _bg__login___sign_up_ek2;
+	private View home_indicator;
+	private View _bg__mode_light__type_default_ek1;
+	private View _bg__notch_ek1;
+	private View _bg__right_side_ek1;
+	private View _bg__battery_ek1;
+	private ImageView rectangle;
+	private ImageView combined_shape;
+	private ImageView rectangle_ek1;
+	private ImageView wifi;
+	private ImageView mobile_signal;
+	private View _bg__left_side_ek1;
+	private View _bg__time_ek1;
+	private ImageView _9_41;
+	private TextView already_have_account__log_in;
+	private ImageView img;
+	private ImageView gradient;
+	private View _bg__button_black_ek1;
+	private View rectangle_107;
+	private TextView sign_up_with_email;
+	private View _bg__apple_button_ek1;
+	private View rectangle_13;
+	private TextView continue_with_apple;
+	private ImageView apple_logo;
+	private View _bg__facebook_button_ek1;
+	private View rectangle_108;
+	private TextView continue_with_facebook;
+	private ImageView facebook_logo;
+	private View _bg__google_button_ek1;
+	private View rectangle_16;
+	private TextView continue_with_google;
+	private ImageView google_logo;
+	private View _bg__or_social_signup_ek1;
+	private TextView or_use_social_sign_up;
+	private ImageView line_56;
+	private ImageView line_57;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.login___sign_up);
+
+		
+		_bg__login___sign_up_ek2 = (View) findViewById(R.id._bg__login___sign_up_ek2);
+		home_indicator = (View) findViewById(R.id.home_indicator);
+		_bg__mode_light__type_default_ek1 = (View) findViewById(R.id._bg__mode_light__type_default_ek1);
+		_bg__notch_ek1 = (View) findViewById(R.id._bg__notch_ek1);
+		_bg__right_side_ek1 = (View) findViewById(R.id._bg__right_side_ek1);
+		_bg__battery_ek1 = (View) findViewById(R.id._bg__battery_ek1);
+		rectangle = (ImageView) findViewById(R.id.rectangle);
+		combined_shape = (ImageView) findViewById(R.id.combined_shape);
+		rectangle_ek1 = (ImageView) findViewById(R.id.rectangle_ek1);
+		wifi = (ImageView) findViewById(R.id.wifi);
+		mobile_signal = (ImageView) findViewById(R.id.mobile_signal);
+		_bg__left_side_ek1 = (View) findViewById(R.id._bg__left_side_ek1);
+		_bg__time_ek1 = (View) findViewById(R.id._bg__time_ek1);
+		_9_41 = (ImageView) findViewById(R.id._9_41);
+		already_have_account__log_in = (TextView) findViewById(R.id.already_have_account__log_in);
+		img = (ImageView) findViewById(R.id.img);
+		gradient = (ImageView) findViewById(R.id.gradient);
+		_bg__button_black_ek1 = (View) findViewById(R.id._bg__button_black_ek1);
+		rectangle_107 = (View) findViewById(R.id.rectangle_107);
+		sign_up_with_email = (TextView) findViewById(R.id.sign_up_with_email);
+		_bg__apple_button_ek1 = (View) findViewById(R.id._bg__apple_button_ek1);
+		rectangle_13 = (View) findViewById(R.id.rectangle_13);
+		continue_with_apple = (TextView) findViewById(R.id.continue_with_apple);
+		apple_logo = (ImageView) findViewById(R.id.apple_logo);
+		_bg__facebook_button_ek1 = (View) findViewById(R.id._bg__facebook_button_ek1);
+		rectangle_108 = (View) findViewById(R.id.rectangle_108);
+		continue_with_facebook = (TextView) findViewById(R.id.continue_with_facebook);
+		facebook_logo = (ImageView) findViewById(R.id.facebook_logo);
+		_bg__google_button_ek1 = (View) findViewById(R.id._bg__google_button_ek1);
+		rectangle_16 = (View) findViewById(R.id.rectangle_16);
+		continue_with_google = (TextView) findViewById(R.id.continue_with_google);
+		google_logo = (ImageView) findViewById(R.id.google_logo);
+		_bg__or_social_signup_ek1 = (View) findViewById(R.id._bg__or_social_signup_ek1);
+		or_use_social_sign_up = (TextView) findViewById(R.id.or_use_social_sign_up);
+		line_56 = (ImageView) findViewById(R.id.line_56);
+		line_57 = (ImageView) findViewById(R.id.line_57);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
